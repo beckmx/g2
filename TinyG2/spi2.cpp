4f205ac31e6b6319c5f29092755f1c6d46827808
matt <matt@msxconsulting.com>
2018-08-06 19:00:47 -0700

    SPI2 command set added, initial master testing

--- TinyG2/spi2.cpp
+++ TinyG2/spi2.cpp
@@ -10,7 +10,6 @@
 
 // Create a SPI instance using CS1 pin for select
 std::shared_ptr<Motate::SPI<kSocket2_SPISlaveSelectPinNumber>> spi2;
-//ALT Motate::SPI<kSocket2_SPISlaveSelectPinNumber> spi2;
 
 // spi2_init: Initializes the SPI2 subsystem
 void spi2_init() {
@@ -20,25 +19,68 @@ void spi2_init() {
   // Set up interrupt on falling edge of SPI_CS2 pin
   spi2_int_pin.setInterrupts(kPinInterruptOnFallingEdge);
 
+
+}
+
+// spi2_cmd: Start a SPI master transfer (read/write) using the provided buffer
+uint8_t spi2_cmd(bool slave_req, uint8_t rnw, uint8_t cmd, uint8_t *data_buf, uint16_t num_data) {
+
+  uint8_t cmd_byte, status = 0xFF;
+
+  // Error checking
+  if ((num_data > 0) && (!data_buf)) {
+    fprintf_P(stderr, PSTR("\nERROR: Invalid data buffer in spi2_cmd\n"));
+    return SPI2_STS_HALT;
+  }
+  if (rnw > 1) {
+    fprintf_P(stderr, PSTR("\nERROR: Read Not Write bit out of range in spi2_cmd (0 - 1)\n"));
+    return SPI2_STS_HALT;
+  }
+  //TODO Check if cmd is invalid
+
+  // Generate command byte by inserting RnW bit to MSB
+  cmd_byte = cmd | (rnw << 7);
+
+  // Write out command byte (slave request, skip this)
+  if (!slave_req) {
+    spi2->write(cmd_byte, false);
+    spi2->flush();  //TODO: Investigate why flush needed
+  }
+
+  // Process data bytes if available
+  if (rnw && (num_data > 0)) {
+    spi2->read(data_buf, num_data, false);  // No-op = 0x00 (default for this function)
+  } else if (num_data > 0) {
+    spi2->write(data_buf, num_data, false);
+  }
+
+  // Read and return status byte
+  status = spi2->read(true, 0xFF);  // Send dummy 0xFF byte on MOSI
+  return status;
 }
 
 void spi2_test() {
 
-  uint8_t buf[4] = {0xDE, 0xAD, 0xBE, 0xEF};
+  uint8_t buf[16] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                     0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
   // Initialize the SPI peripheral and set to this channel
   spi2.reset(new Motate::SPI<kSocket2_SPISlaveSelectPinNumber>(SPI2_MCK_DIV));
-  //ALT spi2.init(SPI2_MCK_DIV, kSPI8Bit | kSPIMode0);
 
   spi2->setChannel();
 
   // Let's write some DEAD BEEF out on the wire...
   //spi2->write(buf, 4, true);
-  spi2->write(buf, 4, true);
+  //spi2->write(buf, 4, true);
 
   // Reads, ignore what slave sent (Aardvark test)
   //spi2->read(true);
   //spi2->read(buf, 4);
+
+  // Try a few sample commands (0x00, 0x01, 0x03)
+  spi2_cmd(false, SPI2_WRITE, SPI2_CMD_RST_ENC_POS, buf, 0);
+  spi2_cmd(false, SPI2_WRITE, SPI2_CMD_START_TOOL_TIP, buf, 0);
+  spi2_cmd(false, SPI2_READ, SPI2_CMD_REQ_ENC_POS, buf, 16);
 }
 
 // SPI2 Slave ISR
--- TinyG2/spi2.h
+++ TinyG2/spi2.h
@@ -7,10 +7,38 @@ extern "C" {
 
 //#include "integer.h"
 
+// General Definitions
 #define SPI2_MCK_DIV  (SystemCoreClock / 84)  // SPI clock divider to generate baud (based on 84MHz MCK)
+
+// Direction (RnW) Bit
+#define SPI2_WRITE 0x00
+#define SPI2_READ  0x01
+
+// SPI2 Command Set
+#define SPI2_CMD_RST_ENC_POS    0x00  // Reset Encoder Positions to Zero
+#define SPI2_CMD_START_TOOL_TIP 0x01  // Start Tool Tip Command
+#define SPI2_CMD_SND_MTR_POS    0x02  // Send Motor Positions
+#define SPI2_CMD_REQ_ENC_POS    0x03  // Request Encoder Positions
+
+#define SPI2_CMD_RD_ENC_POS     0x40  // Read Encoder Position
+#define SPI2_CMD_SET_USER_IO    0x41  // Set User IO
+#define SPI2_CMD_CLR_USER_IO    0x42  // Clear User IO
+#define SPI2_CMD_RD_USER_IO     0x43  // Read User IO
+#define SPI2_CMD_SET_USER_LED   0x44  // Set User LED
+#define SPI2_CMD_CLR_USER_LED   0x45  // Clear User LED
+#define SPI2_CMD_RD_ITR_LOOP    0x46  // Read Interlock Loop
+#define SPI2_CMD_SET_SPIN_LED   0x47  // Set Spindle LEDs
+#define SPI2_CMD_SET_EPS        0x48  // Set Epsilon
+#define SPI2_CMD_RD_ESC_CURR    0x49  // Read ESC Current
+#define SPI2_CMD_FW_VER         0x4A  // Firmware Version
+
+#define SPI2_STS_OK             0x00  // OK Status
+#define SPI2_STS_ERR            0x01  // Error Status
+#define SPI2_STS_HALT           0x02  // Halt
 
 // Function Prototypes
 void spi2_init(void);
+uint8_t spi2_cmd(bool, uint8_t, uint8_t, uint8_t*, uint16_t);
 void spi2_test(void);
 
 #ifdef __cplusplus
