16f9ca77f7f0e273b2effe4d3060d8867ee24163
matt <matt@msxconsulting.com>
2018-09-12 15:24:41 -0700

    Added soft-start timer, temporarily in stepper.cpp

--- TinyG2/hardware.h
+++ TinyG2/hardware.h
@@ -86,7 +86,7 @@ extern "C"{
 #else
 #define SYS_ID_LEN 12
 #endif
-    
+
 /************************************************************************************
  **** ARM SAM3X8E SPECIFIC HARDWARE *************************************************
  ************************************************************************************/
@@ -117,7 +117,7 @@ extern "C"{
  */
 
 /**** Stepper DDA and dwell timer settings ****/
-
+#define FREQUENCY_SS		  1000UL		// PWM soft-start timer frequency, 1ms period (1kHz)
 #define FREQUENCY_DDA		200000.0		// Hz step frequency. Interrupts actually fire at 2x (400 KHz)
 #define FREQUENCY_DWELL		1000UL
 #define FREQUENCY_SGI		200000UL		// 200,000 Hz means software interrupts will fire 5 uSec after being called
@@ -125,11 +125,11 @@ extern "C"{
 /**** Motate Definitions ****/
 
 // Timer definitions. See stepper.h and other headers for setup
-
-timer_number dda_timer_num   = 2;	// stepper pulse generation in stepper.cpp
-timer_number dwell_timer_num = 3;	// dwell timing in stepper.cpp
-timer_number load_timer_num  = 4;	// request load timer in stepper.cpp
-timer_number exec_timer_num  = 5;	// request exec timer in stepper.cpp
+timer_number soft_start_timer_num	= 1;	// pwm soft-start in spindle.cpp
+timer_number dda_timer_num   			= 2;	// stepper pulse generation in stepper.cpp
+timer_number dwell_timer_num 			= 3;	// dwell timing in stepper.cpp
+timer_number load_timer_num  			= 4;	// request load timer in stepper.cpp
+timer_number exec_timer_num  			= 5;	// request exec timer in stepper.cpp
 
 // Pin assignments
 
@@ -145,7 +145,7 @@ static Pin<kSPI0_SCKPinNumber>  spi_sck_pin(kPeripheralA);
 
 //static OutputPin<kSocket1_SPISlaveSelectPinNumber> spi_ss1_pin;
 //static OutputPin<kSocket2_SPISlaveSelectPinNumber> spi_ss2_pin;
-//static OutputPin<kSocket3_SPISlaveSelectPinNumber> spi_ss3_pin;
+static OutputPin<kSocket3_SPISlaveSelectPinNumber> delay_test_pin;
 //static OutputPin<kSocket4_SPISlaveSelectPinNumber> spi_ss4_pin;
 //static OutputPin<kSocket5_SPISlaveSelectPinNumber> spi_ss5_pin;
 //static OutputPin<kSocket6_SPISlaveSelectPinNumber> spi_ss6_pin;
