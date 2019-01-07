d48ffa0dcb41b71577050748e8746660a73dc967
matt <matt@msxconsulting.com>
2019-01-07 15:41:16 -0800

    Changed status codes, added V3 settings stub

--- TinyG2/spi2.h
+++ TinyG2/spi2.h
@@ -46,10 +46,10 @@ typedef enum {
 #define SPI2_CMD_RD_ESC_CURR    0x49  // Read ESC Current
 #define SPI2_CMD_FW_VER         0x4A  // Firmware Version
 
-#define SPI2_STS_OK             0x01  // OK Status
-#define SPI2_STS_ERR            0x02  // Error Status
-#define SPI2_STS_TIMEOUT        0x03  // Timeout Status
-#define SPI2_STS_RETRIES        0x04  // Exceeded Retries Status
+#define SPI2_STS_OK             0x10  // OK Status
+#define SPI2_STS_ERR            0x11  // Error Status
+#define SPI2_STS_TIMEOUT        0x12  // Timeout Status
+#define SPI2_STS_RETRIES        0x13  // Exceeded Retries Status
 
 #define SPI2_STOP_TOOL_TIP      100.0 // WORKAROUND: Unique command to stop tool tip (Z Axis runs 0 to -100 typically)
 
